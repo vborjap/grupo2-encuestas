ddacffbd299a44503ca84e45990faee0b6b89abe
StanDoge <stanlydoge@gmail.com>
2022-10-13 22:57:04 -0600

    Comentarios innecesarios

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,4 +1,3 @@
-//Importando metodo Router de express
 import { Router, urlencoded } from "express";
 import encuentasRoutes from "./encuestas.routes.js";
 import preguntasRoutes from "./preguntas.routes.js"
@@ -6,16 +5,13 @@ import respuestasRoutes from "./respuestas.routes.js"
 import seccionesRoutes from "./secciones.routes.js"
 import usuariosRoutes from "./usuarios.routes.js"
 
-//Igualando método a una constante
 const router = Router();
 
-//Aquí se estarán agregando las rutas y métodos a utilizar
 router.get("/", (req, res) => {
-    // res.send("index");
     res.render('index');
 });
 
-//middleware para about --Anderson Velásquez--VJ21002
+//Middleware para rutas
 // router.use((req, res, next)=>{
 //     if(req.path==("/about")){
 //         next();
@@ -27,7 +23,6 @@ router.get("/", (req, res) => {
 // })
 
 router.get("/about", (req, res) => {
-    // res.send("about");
     res.render("about");
 });
 
