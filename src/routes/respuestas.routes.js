8c1cc1f59e2704514546f410572145d07250d23a
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-10-14 23:37:37 -0600

    Modificacion de rutas en respuestas.routes.js

--- src/routes/respuestas.routes.js
+++ src/routes/respuestas.routes.js
@@ -5,4 +5,10 @@ router.get("/", (req, res) => {
 	res.send("Estas en respuestas")
 });
 
+// Rutas Modulo 5
+router.get("/:id", (req, res) => {
+	const {id} = req.params;
+	res.send("dato: " + id);
+});
+
 export default router;
