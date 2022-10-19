d00dadafbbb22e9c42d37a81794e201612bbc82b
StanDoge <stanlydoge@gmail.com>
2022-10-18 21:08:33 -0600

    Sidebar y rutas agregadas

--- src/routes/encuestas.routes.js
+++ src/routes/encuestas.routes.js
@@ -2,6 +2,12 @@ import { Router} from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
+    res.render('index', {
+        layout: "dashboard"
+    });
+});
+
+router.get("/", (req, res) => {
 	res.send("Estas en encuestas")
 });
 
