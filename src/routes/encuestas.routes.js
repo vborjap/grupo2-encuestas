659337219918b668a1953cf87ed213bb09762556
StanDoge <stanlydoge@gmail.com>
2022-10-20 15:26:04 -0600

    Directorio para esquemas de Mongo

--- src/routes/encuestas.routes.js
+++ src/routes/encuestas.routes.js
@@ -8,5 +8,20 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/",async (req, res) => {
+    let qust = new questions({
+        name: req.body.name,
+        age: req.body.age,
+        sex: req.body.sex
+})
+ 
+try {
+    let savedDocument = await qust.save()
+    res.status(201).json(savedDocument)
+    }
+    catch (err) {
+        res.status(420).send("Error")
+    }
+})
 
 export default router;
