9af1d1c6fc1e134b257239c056b5e15545de3c01
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-10-19 18:28:01 -0600

    Correccion

--- src/routes/respuestas.routes.js
+++ src/routes/respuestas.routes.js
@@ -3,25 +3,12 @@ const router = Router();
 
 // Rutas Modulo 5
 router.get("/", (req, res) => {
-<<<<<<< HEAD
-	
-	// Todo el proceso de obtener los datos en la DB
-
-	res.render("listarRespuestas", {
-		layout: "Dashboard"
-	});
-});
-
-// Rutas Modulo 5
-router.get("/", (req, res) => {
-=======
 	res.render('listarRespuestas', {
 		layout: "dashboard"
 	});
 });
 
 router.get("/:id", (req, res) => {
->>>>>>> d00dadafbbb22e9c42d37a81794e201612bbc82b
 	const {id} = req.params;
 
 	res.render("verRespuestas", {
