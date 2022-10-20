01e7628cca09d5f479eb25a672100d639dbb4967
FatimaAlexandra <49803193+FatimaAlexandra@users.noreply.github.com>
2022-10-19 21:00:27 -0600

    Preguntas

--- src/routes/preguntas.routes.js
+++ src/routes/preguntas.routes.js
@@ -1,6 +1,13 @@
 import { Router} from "express";
 const router = Router();
 
+// Llamamos ruta de preguntas
+router.get("/", (req, res) => {
+	res.render('preguntasView', {
+		layout: "dashboard"
+	});
+});
+
 router.get("/", (req, res) => {
 	res.send("Estas en preguntas")
 });
