4d988d0b4e0a49226d7c37309f5d54fe1f6af665
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-10-19 18:26:38 -0600

    Correccion de merge

--- src/utils/sidebar.handlebars.js
+++ src/utils/sidebar.handlebars.js
@@ -1,21 +1,13 @@
 export const menuList = [
     {
         name: "Inicio",
-<<<<<<< HEAD
-        url: "#inicio",
-=======
         url: "/",
->>>>>>> d00dadafbbb22e9c42d37a81794e201612bbc82b
         icon: "fa-home",
         child: []
     }, 
     {
         name: "Encuestas",
-<<<<<<< HEAD
-        url: "#encuestas",
-=======
         url: "/encuestas",
->>>>>>> d00dadafbbb22e9c42d37a81794e201612bbc82b
         icon: "fa-poll",
         child: []
     },
@@ -27,31 +19,19 @@ export const menuList = [
     },
     {
         name: "Secciones",
-<<<<<<< HEAD
-        url: "#secciones",
-=======
         url: "/secciones",
->>>>>>> d00dadafbbb22e9c42d37a81794e201612bbc82b
         icon: "fa-puzzle-piece",
         child: []
     },
     {
         name: "Preguntas",
-<<<<<<< HEAD
-        url: "#preguntas",
-=======
         url: "/preguntas",
->>>>>>> d00dadafbbb22e9c42d37a81794e201612bbc82b
         icon: "fa-question",
         child: []
     },
     {
         name: "Usuarios",
-<<<<<<< HEAD
-        url: "#usuarios",
-=======
         url: "/usuarios",
->>>>>>> d00dadafbbb22e9c42d37a81794e201612bbc82b
         icon: "fa-users",
         child: []
     }
