160803e0f0971779e64e192de2d0be58e79757fd
AndersonV3 <andersonv3lasquez03@gmail.com>
2022-10-22 11:59:20 -0600

    Vista Crear encuesta

--- src/routes/encuestas.routes.js
+++ src/routes/encuestas.routes.js
@@ -2,18 +2,22 @@ import { Router} from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.render('index', {
+    res.render('crearEncuesta', {
         layout: "dashboard"
     });
 });
 
-// router.post("/",async (req, res) => {
-//     let qust = new questions({
-//         name: req.body.name,
-//         age: req.body.age,
-//         sex: req.body.sex
-// })
- 
+
+
+/*
+router.post("/",async (req, res) => { S
+    let qust = new questions({
+        name: req.body.name,
+        age: req.body.age,
+        sex: req.body.sex
+    
+})
+ */
 // try {
 //     let savedDocument = await qust.save()
 //     res.status(201).json(savedDocument)
