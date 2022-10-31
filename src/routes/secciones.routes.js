a97973a278cecb6e285f76c7178930629910a891
Luis Serpas <sc19013@ues.edu.sv>
2022-10-31 03:53:53 -0600

    Orden rutas de secciones

--- src/routes/secciones.routes.js
+++ src/routes/secciones.routes.js
@@ -4,23 +4,17 @@ import Pregunta from "../models/Pregunta";
 
 const router = Router();
 
-/* router.get("/", (req, res) => {
-	res.send("Estas en secciones")
-}); */
-
-//ruta de vista listar secciones
+//CONSULTAS Y RENDERIZADO DE VISTAS
+//Ruta de vista Listar Secciones
 router.get("/", async (req, res) => {
-
 	const seccion = await Secciones.find().lean()
-
 	res.render('secciones/listarSecciones' ,{
 		layout: "dashboard",
 		seccion: seccion
 	})
-	
 });
 
-//ruta de vista crear seccion
+//Ruta de vista Crear Seccion
 router.get("/addSeccion", async (req, res) => {
 	const pregunta = await Pregunta.find().lean()
 	//const seccion = await Secciones.find({}).populate('preguntas').lean()
@@ -28,21 +22,41 @@ router.get("/addSeccion", async (req, res) => {
 	res.render('secciones/crearSeccion', {
 		layout: "dashboard",
 		preguntas: pregunta,
-		//secciones: seccion
 	});
 });
 
+//Ruta de vista Editar Seccion
+//*********************POR FAVOR INGRESE AQUI EL METODO PARA RENDERIZAR LA VISTA DE EDICION DE SECCION**********************
 
+
+//Ruta de vista Ver Seccion
+router.get("/:id", async (req, res) => {
+	const {id} = req.params
+	const seccion = await Secciones.find({_id: id}).populate('preguntas')
+	//console.log(seccion)
+	res.render('secciones/verSeccion' ,{
+		layout: "dashboard",
+		seccion: seccion
+	})
+});
+
+//MÉTODOS AÑADIR, ELIMINAR Y GUARDAR EDICION
+//Metodo para añadir una nueva seccion
 router.post("/add", async (req, res) => {
-
 	const seccion = Secciones(req.body);
-	
 	const seccionSave = await seccion.save()
-
-	console.log(seccionSave);
-	
-	
+	//console.log(seccionSave);
 	res.redirect("/secciones")
-})
+});
+
+//Método para eliminar una seccion
+router.get("/delete/:id", async (req, res) => {
+	const {id} = req.params
+	await Secciones.findByIdAndDelete(id)
+	res.redirect('/secciones')
+});
+
+//Método para guardar cambios en la edición de una seccion
+//*********************POR FAVOR INGRESE AQUI EL METODO PARA GUARDAR LA EDICION DE SECCION**********************
 
 export default router;
