a05e2eb531d3583539ca955173c2604aea09f81e
Luis Serpas <sc19013@ues.edu.sv>
2022-11-05 03:04:29 -0600

    Ver sección agregada

--- src/routes/secciones.routes.js
+++ src/routes/secciones.routes.js
@@ -33,7 +33,7 @@ router.get("/addSeccion", async (req, res) => {
 router.get("/:id", async (req, res) => {
 	const {id} = req.params
 	const seccion = await Secciones.find({_id: id}).populate('preguntas').lean()
-	console.log(seccion)
+	//console.log(seccion)
 	res.render('secciones/verSeccion' ,{
 		layout: "dashboard",
 		seccion: seccion
