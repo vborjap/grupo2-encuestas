73589c9d4b93edbbce4b7d3bcddc74448be5f6d3
Cristian Antonio Escalante Hernandez <eh181010@ues.edu.sv>
2022-11-06 00:51:32 -0600

    Mapeo de respuestas

--- src/routes/respuestas.routes.js
+++ src/routes/respuestas.routes.js
@@ -1,6 +1,7 @@
 import { Router} from "express";
 const router = Router();
 import registroEncuesta from "../models/encuesta";
+import Respuesta from "../models/respuesta";
 
 // Rutas Modulo 5
 router.get("/", async (req, res) => {
@@ -21,6 +22,42 @@ router.get("/:id", (req, res) => {
 			path: "preguntas"
 		}
 	}).lean();
+
+	let respuestas = await Respuesta.find({idEncuesta: id}).populate({
+		path: "idEncuesta"
+	}).populate({path: "preguntas.idPregunta", select: ["_id", "tipoR"]}).lean();
+
+	console.log(encuesta, respuestas);
+	let respuestasProcesadas = respuestas.map(respuesta => {
+		return respuesta.preguntas.map(pregunta => {
+			if(pregunta.idPregunta.tipoR == "opcion-unica" || pregunta.idPregunta.tipoR == "opcion-multiple"){
+				return Object.fromEntries(new Map([
+					[pregunta.idPregunta._id.toString(), pregunta.respuestas]
+				]));
+			}
+		});
+	});
+	
+	let respuestasMapeadas = respuestasProcesadas.reduce((pre, current, index) => {
+		let valor, llave;
+		for(let i = 0; i < current.length; i++) {
+			valor = Object.values(current[i]);
+			llave = Object.keys(current[i]);
+			if(pre[llave] == undefined) {
+				pre[llave] = valor;
+			}else {
+				pre[llave] = pre[llave].concat(valor);
+			}
+		}
+		return pre;
+	}, {});
+	let datos = {};
+	let resVal, resKey;
+	resVal = Object.values(respuestasMapeadas);
+	resKey = Object.keys(respuestasMapeadas);
+	for(let i = 0; i < resKey.length; i++) {
+		datos[resKey[i]] = resVal[i].reduce((prev, c) => prev.concat(c));
+	}
 
 	res.render("respuestas/show", {
 		layout:"Dashboard",
